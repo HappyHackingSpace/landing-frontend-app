0aa21f33b5f3d72af1fbc43fa3b93a393d403aab
Ozan Tekin <ardilozantekin@gmail.com>
2024-10-07 18:36:24 +0300

    feat: remove old structure

--- app/(landing)/layout.tsx
+++
@@ -1,6 +0,0 @@
-import LandingLayoutView from "@hhs/layouts/landing-layout";
-import { ChildrenProps } from "@hhs/types/layout-props";
-
-export default function LandingLayout({ children }: ChildrenProps) {
-  return <LandingLayoutView>{children}</LandingLayoutView>;
-}
