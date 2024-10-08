983c84bcf49f9d8bb76578ddb07f772656d33022
Ozan Tekin <ardilozantekin@gmail.com>
2024-10-08 22:51:03 +0300

    feat: add useBorderBottomAnimation hook in header and theme switcher

--- layouts/landing-layout/components/landing-header.tsx
+++ layouts/landing-layout/components/landing-header.tsx
@@ -2,21 +2,26 @@
 import * as React from "react";
 import Link from "next/link";
 import ThemeSwitcher from "@hhs/components/custom/theme-switcher";
-import { cn } from "@hhs/utils/cn";
 import useLandingNavigation from "@hhs/hooks/useLandingNavigation";
+import useBorderBottomAnimation from "@hhs/hooks/useBorderBottomAnimation";
+import { Button } from "@hhs/components/shadcn/button";
 
 const LandingHeader = () => {
   return (
     <header className="flex items-center justify-between gap-4">
       <nav className="flex items-center gap-2 flex-wrap grow h-10 px-4">
         {useLandingNavigation().map((item) => (
-          <Link
+          <Button
+            variant="ghost"
+            asChild
             key={item.label}
-            href={item.href}
-            className={cn("capitalize", item.active && "text-orange-500")}
+            className="capitalize relative text-base px-2"
           >
-            {item.label}
-          </Link>
+            <Link href={item.href}>
+              {item.label}
+              {useBorderBottomAnimation(item.active, "landing-header")}
+            </Link>
+          </Button>
         ))}
       </nav>
       <ThemeSwitcher />
