3e8f50a89cd891a777de7097fecf2ccf4f814f53
Ozan Tekin <ardilozantekin@gmail.com>
2024-10-09 22:38:41 +0300

    style: make minor general corrections

--- components/custom/mdx/items/content-section.tsx
+++ components/custom/mdx/items/content-section.tsx
@@ -1,17 +1,21 @@
 import { cn } from "@hhs/utils/cn";
 
 const ContentSection = ({ children }: ChildrenProps) => (
-  <section
+  <article
     className={cn(
-      "prose prose-base dark:prose-invert",
+      "text-pretty",
+      "prose-headings:-mt-2",
+      "prose-hr:border-dashed",
+      "prose-ul:list-none	prose-ul:pl-0",
+      "prose dark:prose-invert",
       "prose-code:rounded prose-code:border prose-code:px-0.5 prose-code:font-normal prose-code:before:content-none prose-code:after:content-none",
       "prose-code:border-base-300 prose-code:bg-base-100 prose-code:text-base-700",
       "dark:prose-code:border-base-500 dark:prose-code:bg-base-700 dark:prose-code:text-base-100",
-      "hover:prose-a:decoration-primary-500 hover:prose-a:decoration-2"
+      "hover:prose-a:decoration-primary-500 hover:prose-a:decoration-2 prose-a:underline-offset-4"
     )}
   >
     {children}
-  </section>
+  </article>
 );
 
 export default ContentSection;
